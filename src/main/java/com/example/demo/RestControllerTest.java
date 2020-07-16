1d6858e761752202e5b3f4b78c009471dce6f223
mopack-Xtreme <mopack@xtremeapp.com.tw>
2020-07-16 11:57:30 +0800

    增加一個 RestControllerTest.java 的測試

---
+++ src/main/java/com/example/demo/RestControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerTest {
+	/*
+	@RequestMapping("/")
+	public String ShowText(){
+		return "This is a RestControllerTest";
+	}
+	*/
+}
